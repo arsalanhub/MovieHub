428c111744cc2945720c352c1f2e58290ec9a831
Mohammad Arsalan <arsalanmohd237@gmail.com>
2021-12-12 13:24:47 +0530

    loader added and card customized

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,16 +3,24 @@ import { movies } from './getMovies'
 
 export default class Banner extends Component {
     render() {
-        console.log({movies})
+        let movie = movies.results[0]
         return (
-            <div class="card" style={{width: "18rem"}}>
-                <img src="" class="card-img-top" alt="..." />
-                <div class ="card-body">
-                <h5 class ="card-title">Card title</h5>
-                <p class ="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class ="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
+            <>
+                {
+                    movie == '' ?
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div> :
+                        <div class="card" style={{ width: "18rem" }}>
+                            <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">{movie.original_title} | {movie.vote_average}</h5>
+                                <p class="card-text">{movie.overview}</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                }
+            </>
         )
     }
 }
