b7a91a6e2ff30801cd6e682bd8aa07270f2cf909
Mohammad Arsalan <arsalanmohd237@gmail.com>
2021-12-12 23:18:20 +0530

    card changes

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -8,15 +8,35 @@ export default class Banner extends Component {
             <>
                 {
                     movie == '' ?
-                        <div class="spinner-border" role="status">
-                            <span class="visually-hidden">Loading...</span>
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
                         </div> :
-                        <div class="card" style={{ width: "18rem" }}>
-                            <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">{movie.original_title} | {movie.vote_average}</h5>
-                                <p class="card-text">{movie.overview}</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="my-3">
+                                        <div className="card banner-card" style={{ width: "18rem" }}>
+                                            <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top" alt="..." />
+                                            <div className="card-body">
+                                                <h5 className="card-title banner-title">{movie.original_title} | {movie.vote_average}</h5>
+                                                <p className="card-text">{movie.overview}</p>
+                                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="my-3">
+                                        <div className="card banner-card" style={{ width: "18rem" }}>
+                                            <img src={`https://image.tmdb.org/t/p/original${movies.results[1].backdrop_path}`} className="card-img-top" alt="..." />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{movies.results[1].original_title} | {movies.results[1].vote_average}</h5>
+                                                <p className="card-text">{movies.results[1].overview}</p>
+                                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                 }
